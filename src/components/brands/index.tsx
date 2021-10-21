84815461cce12d019e8379c3db29d47186ece2b4
Martin Blackburn <martin.blackburn@skybettingandgaming.com>
2021-10-21 21:05:22 +0100

    added more brands

--- src/components/brands/index.tsx
+++ src/components/brands/index.tsx
@@ -9,8 +9,11 @@ const Brands = () => {
             <h1>Brands I've work with:</h1>
 
             <div className="brands__images">
+                <img src="/images/brands/bbc.png" alt="bbc logo" />
+                <img src="/images/brands/gripple.png" alt="gripple logo" />
                 <img src="/images/brands/guardian jobs.png" alt="guardian jobs logo" />
                 <img src="/images/brands/lapicida.png" alt="lapicida logo" />
+                <img src="/images/brands/national club golfer.png" alt="national club golfer logo" />
                 <img src="/images/brands/new scientist.png" alt="wren kitchens logo" />
                 <img src="/images/brands/sky betting.png" alt="sky betting logo" />
                 <img src="/images/brands/washington post.png" alt="washington post logo" />
