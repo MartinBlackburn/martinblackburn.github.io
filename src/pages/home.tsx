1c995b5e478e7be4bf17357e4ab432a89b9e0dd0
Martin Blackburn <martin.blackburn@skybettingandgaming.com>
2024-06-26 15:38:07 +0100

    removed brands and about from home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,8 @@
 //libraries
 import React from "react";
 
-// components
-import About from "../components/about";
-import Brands from "../components/brands";
-
 const HomePage = () => {
-    return (
-        <React.Fragment>
-            <About />
-            <Brands />
-        </React.Fragment>
-    );
+    return <React.Fragment></React.Fragment>;
 };
 
 export default HomePage;
